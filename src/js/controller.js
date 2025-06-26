7a8103e989f7cf05856436a0981589de366982c9
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-06-26 17:39:26 +0400

    feat: implement recipe servings update functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -57,8 +57,17 @@ const controlPagination = goToPage => {
   paginationView.render(model.state.search);
 };
 
+const constrolServings = newServings => {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = () => {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(constrolServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
